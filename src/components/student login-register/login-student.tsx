27c02353c512476ef5ff9b39885a1d8494e8ff88
prashant <prash2402@gmail.com>
2023-12-06 23:04:32 +0530

    Fix , routed student login

--- src/components/student login-register/login-student.tsx
+++ src/components/student login-register/login-student.tsx
@@ -1,6 +1,33 @@
 import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+
 
 const StudentLogin = () => {
+  const router = useRouter();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e: any) => {
+    e.preventDefault();
+
+    // Basic validation
+    if (!email || !password) {
+      alert('Please enter both email and password.');
+      return;
+    }
+    // Replace this with your actual login logic
+    // For now, let's consider a dummy authentication
+    if (email === 'admin@example.com' && password === 'adminpassword') {
+      // Successful login, redirect to admin dashboard
+      router.push('/admin-dashboard');
+    } else {
+      // Invalid credentials
+      alert('Invalid email or password. Please try again.');
+    }
+  };
   return (
     <>
       <div className="w-full bg-white xl:mx-0 md:mx-0 mx-5   rounded-lg md:w-1/3 xl:w-1/3">
@@ -11,11 +38,17 @@ const StudentLogin = () => {
                 Email
               </label>
               <div className="relative">
-                <input
-                  type="email"
-                  placeholder="Enter your email"
-                  className="md:w-11/12 w-full xl:w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none"
-                />
+              <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Enter your email"
+                    className="md:w-11/12 w-full xl:w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none"
+                  />
 
               </div>
             </div>
@@ -25,11 +58,17 @@ const StudentLogin = () => {
                 Password
               </label>
               <div className="relative">
-                <input
-                  type="password"
-                  placeholder="password"
-                  className="md:w-11/12 w-full xl:w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-4 outline-none focus:border-primary focus-visible:shadow-none"
-                />
+              <input
+                     id="password"
+                     name="password"
+                     type="password"
+                     autoComplete="current-password"
+                     required
+                     value={password}
+                     onChange={(e) => setPassword(e.target.value)}
+                    placeholder="password"
+                    className="md:w-11/12 w-full xl:w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-4 outline-none focus:border-primary focus-visible:shadow-none"
+                  />
 
               </div>
             </div>
@@ -37,8 +76,9 @@ const StudentLogin = () => {
             <div className="mb-5">
               <input
                 type="submit"
+                onClick={handleLogin}
                 value="Sign In"
-                className="w-full cursor-pointer rounded-lg  bg-sdmis-primary-600 p-4 text-white transition hover:bg-opacity-90"
+                className="w-full cursor-pointer rounded-lg text-lg bg-sdmis-primary-600 p-4 text-white transition hover:bg-opacity-90"
               />
             </div>
 
